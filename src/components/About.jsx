41ed43b5c19e0ff25717c200ac0b67a62bd48b76
Jeanmax <jmaxdes@gmail.com>
2019-12-30 12:24:35 -0500

    Udpated icon pixels  from 48 to 96

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
-import html from '../images/icons-html-5.png'
-import css from '../images/icons-css3-48.png'
-import js from '../images/icons-javascript-48.png'
-import react from '../images/icons-react-48.png'
-import postgres from '../images/icons-postgresql-48.png'
-import ruby from '../images/icons-ruby-48.png'
-import git from '../images/icons-git-48.png'
-import github from '../images/icons-github-48.png'
-import node from '../images/icons-nodejs-48.png'
+import html from '../images/icons-html-5-96.png'
+import css from '../images/icons-css3-96.png'
+import js from '../images/icons-javascript-96.png'
+import react from '../images/icons-react-native-96.png'
+import postgres from '../images/icons-postgresql-96.png'
+import ruby from '../images/icons-ruby-96.png'
+import git from '../images/icons-git-96.png'
+import github from '../images/icons-github-96.png'
+import node from '../images/icons-nodejs-96.png'
+
 
 
 const About = () => {
@@ -17,20 +18,28 @@ const About = () => {
             <div className='icons'>
                 <img src={html} alt="" />
                 <p>HTML</p>
+
                 <img src={css} alt="" />
                 <p>CSS</p>
+
                 <img src={js} alt="" />
                 <p>JavaScript</p>
+
                 <img src={react} alt="" />
                 <p>React.js</p>
+
                 <img src={postgres} alt="" />
                 <p>Express.js</p>
+
                 <img src={ruby} alt="" />
                 <p>Ruby</p>
+
                 <img src={node} alt="" />
                 <p>Node.js</p>
+
                 <img src={git} alt="" />
                 <p>Git</p>
+
                 <img src={github} alt="" />
                 <p>Github</p>
             </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
+import profile from '../images/profile.jpg'
 
 export default class Header extends Component {
     render() {
         return (
             <div className="header">
-                <h1>Welcome to my portfolio</h1><p>I'm a junior Web developer located in the greater new york city area</p>
+                <h1>Welcome to my portfolio</h1>
+                <img src={profile} alt=''/>
+                <p>I'm a junior Web developer located in the greater new york city area</p>
             </div>
         )
     }
